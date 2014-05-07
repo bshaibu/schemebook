324962524608795ab74a7de43099a71c59bcb490
bshaibu <benjaminshaibu@gmail.com>
2014-05-06 23:11:28 -0400

    added tempalte files and Iron Router routing

--- client/schemebook.js
+++ client/schemebook.js
@@ -1,19 +1,11 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to schemebook.";
-  };
-
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  //Routing
+  Router.map(function() {
+    this.route('home', {path: '/'});
+    this.route('class');
+    this.route('unit');
+    this.route('lesson');
+    this.route('profile');
+    this.route('calendar');
   });
 }

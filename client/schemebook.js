476f844b0a3177fbd81a2bf30e4d4574d1390871
bshaibu <benjaminshaibu@gmail.com>
2014-05-06 23:13:15 -0400

    updated routes

--- client/schemebook.js
+++ client/schemebook.js
@@ -3,7 +3,7 @@ if (Meteor.isClient) {
   Router.map(function() {
     this.route('home', {path: '/'});
     this.route('class');
-    this.route('unit');
+    this.route('classes');
     this.route('lesson');
     this.route('profile');
     this.route('calendar');

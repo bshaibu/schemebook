c7fb70aa517089ae9164864d82ecaf3b80fdcf76
Benjamin Shaibu <benjaminshaibu@gmail.com>
2014-05-08 18:07:13 -0400

    added menubar, updated routing

--- client/schemebook.js
+++ client/schemebook.js
@@ -9,5 +9,28 @@ if (Meteor.isClient) {
     this.route('calendar');
     this.route('login');
     this.route('register');
+    this.route('notFound', {path: "*"});
   });
 }
+
+/*
+
+//Routing for Individual Classes
+
+this.route('classDetail', { 
+  path: '/class/:_id',
+  data: function() { return Classes.findOne(this.params._id); }
+});
+
+//put last
+
+Router.configure({
+//layoutTemplate: 'layout',
+notFoundTemplate: 'findesikke',
+});
+
+this.routte('notFound', {
+  path: "*"
+});
+
+*/

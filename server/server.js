774311ed41a03c8a831eecf5802ec167dea4535a
Benjamin Shaibu <benjaminshaibu@gmail.com>
2014-05-09 00:45:07 -0400

    added server filler

--- server/server.js
+++ server/server.js
@@ -1,5 +1,31 @@
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+
+    //User: name, username, school, password, classes
+    Meteor.startup(function () {
+        // code to run on server at startup
+        //Add Dummy Data
+        if (Meteor.users.find().count() === 0) {
+            console.log("no users buckaroo");
+        }
+        if (Classes.find().count() === 0) {
+
+            Classes.insert({name: "6.813", teacher: "Rob Miller", school: "MIT", units:"1"});
+        }
+        if (Units.find().count() === 0) {
+            Units.insert( { name: "Learnability", number: "1", class:"6.813", lessons: "{1,2,3}" } );
+        }
+        if (Lessons.find().count() === 0) {
+            Lessons.insert({
+                number:"1", 
+                name:"", 
+                unit:"Learnability", 
+                objective:"", 
+                reading:"", 
+                lecture_notes:"", 
+                quiz:"", 
+                comments: "",
+                scheduled_date: ""
+            });
+        }
   });
 }
